1b3e41ae5096d5121bd53f16e75a810b1d25de3a
YaroslavRom <yaroslavromaniuk@gmail.com>
2021-08-04 16:28:51 +0300

    start of repository

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
+
+gulp.task('sass-compile', function (){
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./css/'))
+})
+
+gulp.task('watch', function (){
+    gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'))
+})
+
+
+
